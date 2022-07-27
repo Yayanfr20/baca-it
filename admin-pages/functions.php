07272052e3dc0eb8bd7c36d53b7fbe6fe3cf027a
Yayanfr20 <yayanfathurohman20>
2022-07-27 23:16:05 +0700

    repair upload

--- admin-pages/functions.php
+++ admin-pages/functions.php
@@ -55,87 +55,94 @@ return mysqli_affected_rows($conn);
 // upload pdf
 
 function uploadpdf() {
-    $namaFile = $_FILES['pdf']['name'];
-    $ukuranFile = $_FILES['pdf']['size'];
-    $error = $_FILES['pdf']['error'];
-    $tmpName = $_FILES['pdf']['tmp_name'];
 
-    if( $error === 4) {
-        echo"<script>
-                alert('pilih gambar terlebih dahulu');
-        </script>";
-    return false;
-    }
+	$namaFile = $_FILES['pdf']['name'];
+	$ukuranFile = $_FILES['pdf']['size'];
+	$error = $_FILES['pdf']['error'];
+	$tmpName = $_FILES['pdf']['tmp_name'];
 
-    $ektensiPdfValid = ['pdf'];
-    $ektensiPdf = strtolower(end($ektensiPdfValid));
+	// cek apakah tidak ada gambar yang diupload
+	if( $error === 4 ) {
+		echo "<script>
+				alert('pilih file terlebih dahulu!');
+			  </script>";
+		return false;
+	}
 
-    if(!in_array($ektensiPdf, $ektensiPdfValid) ) {
-        echo "<script>
-            alert('yang kamu upload bukan pdf!');
-        </script>";
-        return false;
-    }
+	// cek apakah yang diupload adalah gambar
+	$ekstensiGambarValid = ['zip', 'rar'];
+	$ekstensiGambar = explode('.', $namaFile);
+	$ekstensiGambar = strtolower(end($ekstensiGambar));
+	if( !in_array($ekstensiGambar, $ekstensiGambarValid) ) {
+		echo "<script>
+				alert('yang anda upload bukan file!');
+			  </script>";
+		return false;
+	}
 
+	// cek jika ukurannya terlalu besar
+	if( $ukuranFile > 100000000 ) {
+		echo "<script>
+				alert('ukuran file terlalu besar!');
+			  </script>";
+		return false;
+	}
 
-    if ( $ukuranFile > 100000000 ) {
-        echo "<script>
-            alert('ukuran pdf terlalu besar');
-        </script>";
-        return false;
-    }
+	// lolos pengecekan, gambar siap diupload
+	// generate nama gambar baru
+	$namaFileBaru = uniqid();
+	$namaFileBaru .= '.';
+	$namaFileBaru .= $ekstensiGambar;
 
+	move_uploaded_file($tmpName, 'pdf/' . $namaFileBaru);
 
-    $namaFileBaru = uniqid();
-    $namaFileBaru .= ".";
-    $namaFileBaru .= $ektensiPdf;
-
-    move_uploaded_file($tmpName, 'pdf/' . $namaFileBaru);
-    return$namaFileBaru;
-
-
-
+	return $namaFileBaru;
 }
 
 // upload cover
 function uploadcover() {
+
 	$namaFile = $_FILES['cover']['name'];
 	$ukuranFile = $_FILES['cover']['size'];
 	$error = $_FILES['cover']['error'];
 	$tmpName = $_FILES['cover']['tmp_name'];
 
-	if( $error === 4) {
-		echo"<script>
-				alert('pilih gambar terlebih dahulu');
-		</script>";
-	return false;
-	}
-
-	$ektensiGambarValid = ['jpg', 'jpeg', 'png' ];
-	$ektensiGambar = strtolower(end($ektensiGambarValid));
-
-	if(!in_array($ektensiGambar, $ektensiGambarValid) ) {
+	// cek apakah tidak ada gambar yang diupload
+	if( $error === 4 ) {
 		echo "<script>
-			alert('yang kamu upload bukan gambar!');
-		</script>";
+				alert('pilih gambar terlebih dahulu!');
+			  </script>";
 		return false;
 	}
 
-
-	if ( $ukuranFile > 1000000 ) {
+	// cek apakah yang diupload adalah gambar
+	$ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+	$ekstensiGambar = explode('.', $namaFile);
+	$ekstensiGambar = strtolower(end($ekstensiGambar));
+	if( !in_array($ekstensiGambar, $ekstensiGambarValid) ) {
 		echo "<script>
-			alert('ukuran gambar terlalu besar');
-		</script>";
+				alert('yang anda upload bukan gambar!');
+			  </script>";
 		return false;
 	}
 
+	// cek jika ukurannya terlalu besar
+	if( $ukuranFile > 10000000 ) {
+		echo "<script>
+				alert('ukuran gambar terlalu besar!');
+			  </script>";
+		return false;
+	}
 
+	// lolos pengecekan, gambar siap diupload
+	// generate nama gambar baru
 	$namaFileBaru = uniqid();
-	$namaFileBaru .= ".";
-	$namaFileBaru .= $ektensiGambar;
+	$namaFileBaru .= '.';
+	$namaFileBaru .= $ekstensiGambar;
 
 	move_uploaded_file($tmpName, 'cover/' . $namaFileBaru);
-	return$namaFileBaru;
+
+	return $namaFileBaru;
 }
 
     ////////////////// function registration /////////////////
